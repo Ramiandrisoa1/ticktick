6439cdad17b4237784d4e4eacb0b552c1060e047
Ramiandrisoa1 <ramiandrisoamicka@gmail.com>
2022-07-20 11:46:29 +0300

    fix txt to json

--- tick-front/src/page/home.jsx
+++ tick-front/src/page/home.jsx
@@ -6,21 +6,39 @@ function Alarme() {
     fetch(file)
       .then((r) => r.text())
       .then((text) => {
-        text.split('\n\n').map((entry) => {
-          const obj = [];
-          entry.split('\n').forEach((keyValue) => {
-            obj.push(keyValue);
-          });
-          console.log(obj);
+        const textSplit = text.split('\n');
+        const finalValue = [];
+        textSplit.map((text, index) => {
+          const obj = {};
+          if (text.includes('SAE') && !text.includes('>')) {
+            const textTmp = text
+              .split(/((?:\w+ ){1})/g)
+              .map((data) => {
+                if (data.replace(/\s/g, '')) {
+                  return data.replace(/\s/g, '');
+                }
+              })
+              .filter((data) => data !== undefined);
+
+            const value = textSplit[index + 1]
+              .split(/((?:\w+ ){1})/g)
+              .map((data) => {
+                if (data.replace(/\s/g, '') !== '') {
+                  return data.replace(/\s/g, '');
+                }
+              })
+              .filter((data) => data !== undefined);
+            for (let i = 0; i < 7; i++) {
+              obj[textTmp[i]] = value[i];
+            }
+            finalValue.push(obj);
+          }
         });
+        console.log('final: ', finalValue);
       });
   }, []);
 
-  return (
-    <div className='Alarme'>
-      <button>ok</button>
-    </div>
-  );
+  return <div className='Alarme'></div>;
 }
 
 export default Alarme;
